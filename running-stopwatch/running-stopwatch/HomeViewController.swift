65e1eedcde00bfa5c1b15cc6f84bf4af3221fcfe
Mateus de Campos <mateuscampos_nba@hotmail.com>
2018-01-27 19:18:30 -0200

    configura view coding

---
+++ running-stopwatch/running-stopwatch/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  running-stopwatch
+//
+//  Created by Mateus Campos on 27/01/2018.
+//  Copyright © 2018 Mateus Campos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HomeViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
